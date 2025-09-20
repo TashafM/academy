2bc46cee8ce61642519e012307d3385fdabe509c
TashafM <thecomputerpitch@gmail.com>
2025-09-20 18:54:59 +0530

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>CodeScriptors Academy</div>
+    <div className='text-5xl text-center flex items-center justify-center h-screen animate-bounce'>CodeScriptors <br/> Academy</div>
   )
 }
 
